4365eb64aa47cf63afe4b6bc1b24740f5da4788d
Boretto22 <pozotoni08@gmail.com>
2025-09-26 14:34:26 +0200

    .

---
+++ src/FutbolManager.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class FutbolManager extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("FutbolManager");
+    }
+
+    public void settings(){
+        fullScreen();
+    }
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+}
